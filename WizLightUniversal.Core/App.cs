524436746d5ea2331bcf95dd635c3391a89cab47
Christopher Madrigal <c4madrig@ucsd.edu>
2020-07-07 02:38:29 -0700

    Add project files.

---
+++ WizLightUniversal.Core/App.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace WizLightUniversal.Core
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Debug.WriteLine("Application started");
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            Debug.WriteLine("Application sleeps");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Debug.WriteLine("Application resumes");
+        }
+    }
+}
