4a7a23699b71e710f2bbbc25482706ca0775716b
Christopher Madrigal <c4madrig@ucsd.edu>
2020-07-29 14:38:50 -0700

    Code cleanup of Windows

--- WizLightUniversal.Windows/WinPreferencesProvider.cs
+++ WizLightUniversal.Windows/WinPreferencesProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
-using System.Windows.Forms;
-using WizLightUniversal.Core;
+﻿using WizLightUniversal.Core;
 
 namespace WizLightUniversal.Windows
 {
